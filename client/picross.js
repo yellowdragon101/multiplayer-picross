f6ac0f1111f2ef432e78d4f368c67ecc88aed5a2
yellow <donte_beck@yahoo.com>
2019-09-22 22:51:16 -0700

    you can now join a room

--- client/picross.js
+++ client/picross.js
@@ -1,18 +1,18 @@
 const socket = io();
-let currBoard;
-let answerCount;
 
 function createRoom() {
 	socket.emit('createRoom');
 }
 
-function joinRoom() {
-	socket.emit('joinRoom');
+function joinRoom(evn) {
+	const target = evn.target;
+	id = target.dataset.roomid;
+	socket.emit('joinRoom', id);
 }
 
-socket.on('joinRoom', function() {
+socket.on('initRoom', function(top, side) {
+	actuallyMakeBoard(top, side)
 	toVisible();
-	startGame();
 });
 
 socket.on('updateRooms', function(roomList) {
@@ -21,31 +21,17 @@ socket.on('updateRooms', function(roomList) {
 	for(key of roomList) {
 		const roomItem = document.createElement('li');
 		roomItem.textContent = key;
-		roomItem.onclick = joinRoom;
+		roomItem.dataset.roomid = key;
+		roomItem.addEventListener('click', joinRoom);
 		rooms.appendChild(roomItem);
 	}
 });
 
-function startGame() {
-	// let size = document.getElementById("size").value;
-	let size = 10;
-    let output = document.getElementById("err");
-    output.textContent = "";
-	/*
-	if(size == null || size <= 0) {
-        output.style.color = "red";
-        output.textContent = "Enter a board size.";
-        return;
-    }
-	*/
-	currBoard = makeBoard(size);
-	const top = getTop(currBoard);
-	const side = getSide(currBoard);
-	actuallyMakeBoard(currBoard, top, side);
-    answerCount = 0;
-}
+socket.on('updateRoom', function() {
+	
+});
 
-function actuallyMakeBoard(board, top, side) {
+function actuallyMakeBoard(top, side) {
 	const table = document.getElementById("table");
 	table.innerHTML = "";
 
@@ -55,12 +41,12 @@ function actuallyMakeBoard(board, top, side) {
 		const cell = topRow.insertCell();
 		cell.innerText = stub.length > 0 ? stub.join('\n') : '0';
 	}
-	for(let r=0;r<board.length;r++) {
+	for(let r=0;r<10;r++) {
 		const row = table.insertRow();
 		const stub = row.insertCell();
 		stub.innerText = side[r].length > 0 ? side[r].join(',') : '0';
 
-		for(let c=0;c<board[0].length;c++) {
+		for(let c=0;c<10;c++) {
             cell = row.insertCell();
             cell.dataset.y = `${r}`
             cell.dataset.x = `${c}`
@@ -71,54 +57,15 @@ function actuallyMakeBoard(board, top, side) {
 	}
 }
 
-function makeBoard(size) {
-	let board = [];
-	for(let y=0;y<size;y++) {
-		board.push([]);
-		for(let x=0;x<size;x++) {
-			board[y][x] = randInt();
-		}
-	}
-	return board;
-}
-
-function randInt() {
-	return Math.round(Math.random());
-}
-
-function getTop(grid) {
-	const cols = grid[0].map((col,i) => grid.map((row) => row[i]));
-	return cols.map((col) => stubValues(col));
-}
-
-function getSide(grid) {
-	const rows = grid;
-	return rows.map((row) => stubValues(row));
-}
-
-function stubValues(arr) {
-	const values = [arr[0]];
-    for(let i=1;i<arr.length;i++) {	
-        arr[i] == 0 ? values.push(arr[i]) : values[values.length - 1] += 1;
-	}
-	return filterZero(values);
-}
-
-function filterZero(arr) {
-	return arr.filter((x) => x != 0);
-}
-
 function handleClick(evn) {
 	const target = evn.target;
 	if(target.classList.contains("unclicked")) {
-        if(currBoard[target.dataset.y][target.dataset.x] == 1) answerCount += 1;
 		target.classList.replace("unclicked", "clicked");
 	}
 	else if(target.classList.contains("right-clicked")) {
 		target.classList.replace("right-clicked", "clicked");
 	}
 	else {
-        if(currBoard[target.dataset.y][target.dataset.x] == 1) answerCount -= 1;
 		target.classList.replace("clicked", "unclicked");
     }
 }
@@ -141,25 +88,6 @@ function handleRightClick(evn) {
 }
 
 function toVisible() {
-	const board = document.getElementById("board");
-	const room = document.getElementById("room");
-	if(board.classList.contains("hid")) {
-		board.classList.toogle('hid', bool);
-	}
-	else if(room.classList.contains("hid")) {
-		room.classList.toogle('hid', bool);
-	}
-}
-
-function checkAnswer() {
-    let count = 0;
-    const output = document.getElementById("err");
-
-	for(row of currBoard) {
-        for(val of row) {
-            if(val == 1) count += 1
-        }
-    }
-    output.style.color = "black";
-    output.textContent = `${count - (count - answerCount)}/${count} tiles correct.`;
+	document.getElementById("board").classList.toggle('hid');
+	document.getElementById("room").classList.toggle('hid');
 }
--- server/index.js
+++ server/index.js
@@ -16,20 +16,28 @@ io.on('connection', function(socket) {
 	console.log('a user connected');
 	io.emit('updateRooms', Object.keys(rooms));
 	socket.on('updateRooms', function(roomList) {
-		roomList = rooms;
 		io.emit(Object.keys(roomList));
 	});
 	socket.on('createRoom', function(roomNum) {
 		roomNum = Object.keys(rooms).length + 1;
 		console.log(`room #${roomNum} created`);
-		rooms[roomNum] = {players: [socket]};
+		rooms[roomNum] = {players: [socket], board: [], top: [], side: []};
+
+		let board = rooms[roomNum]["board"];
+		board = makeBoard(10);
+		rooms[roomNum]["top"] = getTop(board);
+		rooms[roomNum]["side"] = getSide(board);
+		
 		io.emit('updateRooms', Object.keys(rooms));
 	});
-	socket.on('joinRoom', function() {
-		console.log("user joined room");
+	socket.on('joinRoom', function(roomID) {
+		console.log(`user joined room ${roomID}`);
+		io.emit('initRoom', rooms[roomID]["top"], rooms[roomID]["side"]);
+	});
+	socket.on('initRoom', function(top, side) {
+		io.emit(top, side);
 	});
 	socket.on('updateRoom', function() {
-
 	});
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
@@ -39,3 +47,36 @@ io.on('connection', function(socket) {
 http.listen(port, function() {
 	console.log(`listening on *:${port}`);
 });
+
+function makeBoard(size) {
+	let board = [];
+	for(let y=0;y<size;y++) {
+		board.push([]);
+		for(let x=0;x<size;x++) {
+			board[y][x] = Math.round(Math.random());
+		}
+	}
+	return board;
+}
+
+function getTop(grid) {
+	const cols = grid[0].map((col,i) => grid.map((row) => row[i]));
+	return cols.map((col) => stubValues(col));
+}
+
+function getSide(grid) {
+	const rows = grid;
+	return rows.map((row) => stubValues(row));
+}
+
+function stubValues(arr) {
+	const values = [arr[0]];
+    for(let i=1;i<arr.length;i++) {	
+        arr[i] == 0 ? values.push(arr[i]) : values[values.length - 1] += 1;
+	}
+	return filterZero(values);
+}
+
+function filterZero(arr) {
+	return arr.filter((x) => x != 0);
+}
